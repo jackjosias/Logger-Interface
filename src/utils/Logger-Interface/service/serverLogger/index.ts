0630d35d64db546c2020872d57edf0b588973f54
Jack-Josias <85123055+jackjosias@users.noreply.github.com>
2024-11-03 18:09:42 +0100

    Have fun and Enjoy, it's for everyone and I hope this Repo will be useful to you! If so, add a Star to help keep it maintained.

--- src/utils/Logger-Interface/service/serverLogger/index.ts
+++ src/utils/Logger-Interface/service/serverLogger/index.ts
@@ -1,17 +1,16 @@
-
-
-import { ServerLogger, LogEntry, ILogger } from './serverLogger';
-
+import { ServerLogger, LogEntry, ILogger } from "./serverLogger";
 
 let logger: ILogger;
 
-
-if (typeof window === 'undefined')  
+if (typeof window === "undefined") 
 {
-    // On est côté serveur
-    logger = ServerLogger.getInstance();
-
+  // On est côté serveur
+  logger = ServerLogger.getInstance();
 } 
+else 
+{
+  throw new Error("Le logger ne peut être utilisé que côté serveur.");
+}
 
 export { logger };
 export type { LogEntry, ILogger };
